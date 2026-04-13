6573ce4133e52a0296c90b1e9326d13803b21812
Lee <135443673+Lee-Abraham@users.noreply.github.com>
2026-04-12 18:00:28 -0600

    update

--- backend/shared/db.py
+++ backend/shared/db.py
@@ -1,17 +1,12 @@
-from azure.identity import DefaultAzureCredential
+import os
 import psycopg2
+from psycopg2.extras import RealDictCursor
+
+DATABASE_URL = os.environ["DATABASE_URL"]
 
 def get_connection():
-    credential = DefaultAzureCredential()
-
-    token = credential.get_token(
-        "https://ossrdbms-aad.database.windows.net/.default"
-    ).token
-
     return psycopg2.connect(
-        host="docsmart-postgres.postgres.database.azure.com",
-        dbname="docsmartdb",
-        user="docsmart-backend",   #Entra DB role
-        password=token,            #token replaces password
-        sslmode="require"
+        DATABASE_URL,
+        sslmode="require",
+        cursor_factory=RealDictCursor
     )
